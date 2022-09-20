f8800d61d685ed1568485cea8a69e9bc7d798fa0
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-20 09:56:18 -0400

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("button").addEventListener("click", function(){
+    document.getElementById("pop").style.display = "flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("pop").style.display = "none";
+})
